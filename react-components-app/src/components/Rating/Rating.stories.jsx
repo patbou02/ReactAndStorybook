630109e90864ea9b24ff31aaaf7e2c7a7247742f
Patrick Boutin <patrick.boutin@roberthalf.com>
2022-07-18 23:47:59 -0700

    Created Rating component, imported React icon dependencies and added to SB.

---
+++ react-components-app/src/components/Rating/Rating.stories.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Rating } from './Rating';
+
+export default {
+  title: 'Example/Rating',
+  component: Rating,
+};
+
+const Template = (args) => <Rating {...args} />;
+
+export const FiveStars = Template.bind({});
+FiveStars.args = {
+  number: 5,
+};
