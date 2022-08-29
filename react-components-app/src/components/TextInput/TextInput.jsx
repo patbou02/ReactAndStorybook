ea0c77c0c63d4e1eb4251d153c3481e38fd8c1a0
Patrick Boutin <patrick.boutin@roberthalf.com>
2022-08-29 01:40:26 -0700

    Changed TextInput component from being an uncontrolled component to being a controlled component.

--- react-components-app/src/components/TextInput/TextInput.jsx
+++ react-components-app/src/components/TextInput/TextInput.jsx
@@ -1,10 +1,20 @@
 import PropTypes from 'prop-types';
+import { useState } from "react";
 
 const TextInput = ({name, type, label, placeholder}) => {
+  const [value, setValue] = useState('');
+  const handleChange = (e) => setValue(e.target.value);
+
   return (
     <div>
       <label htmlFor={name}>{label}</label>
-      <input type={type} name={name} placeholder={placeholder} />
+      <input
+        type={type}
+        name={name}
+        value={value}
+        placeholder={placeholder}
+        onChange={handleChange}
+      />
     </div>
   );
 };
